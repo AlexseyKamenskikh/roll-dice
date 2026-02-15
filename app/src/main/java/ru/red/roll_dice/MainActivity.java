ff4c6ca51bdd20d4f5148f57119e2d84c820b31b
Alexsey Kamenskikh <aakamenskikh@mail.ru>
2026-02-15 20:21:21 +0500

    release 1.0.0

--- app/src/main/java/ru/red/roll_dice/MainActivity.java
+++ app/src/main/java/ru/red/roll_dice/MainActivity.java
@@ -1,6 +1,10 @@
 package ru.red.roll_dice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,66 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView dice1ImageView;
+    private ImageView dice2ImageView;
+    private Button rollButton;
+    private TextView resultTextView;
+    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        dice1ImageView = findViewById(R.id.dice1ImageView);
+        dice2ImageView = findViewById(R.id.dice2ImageView);
+        rollButton = findViewById(R.id.rollButton);
+        resultTextView = findViewById(R.id.resultTextView);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
         });
+    }
+
+    private void rollDice() {
+        int dice1Value = random.nextInt(6) + 1;
+        int dice2Value = random.nextInt(6) + 1;
+
+        setDiceImage(dice1ImageView, dice1Value);
+        setDiceImage(dice2ImageView, dice2Value);
+
+        int sum = dice1Value + dice2Value;
+        resultTextView.setText("Выпало: " + dice1Value + " и " + dice2Value +
+                " (Сумма: " + sum + ")");
+    }
+
+    private void setDiceImage(ImageView diceImageView, int value) {
+        switch (value) {
+            case 1:
+                diceImageView.setImageResource(R.drawable.dice_1_1920);
+                break;
+            case 2:
+                diceImageView.setImageResource(R.drawable.dice_2_1920);
+                break;
+            case 3:
+                diceImageView.setImageResource(R.drawable.dice_3_1920);
+                break;
+            case 4:
+                diceImageView.setImageResource(R.drawable.dice_4_1920);
+                break;
+            case 5:
+                diceImageView.setImageResource(R.drawable.dice_5_1920);
+                break;
+            case 6:
+                diceImageView.setImageResource(R.drawable.dice_6_1920);
+                break;
+        }
     }
 }
